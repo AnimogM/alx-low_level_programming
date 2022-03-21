47a680a079d7d261aebf0fa6388e2051b36a228b
Gomina Maryam <gominamryam@gmail.com>
2022-03-21 06:35:05 -0700

    update

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_rev - prints reversed string;
+ *@str: string to be printed
+ */
+
+void print_rev(char *str)
+{
+	int i;
+	
+	int len;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	i = len;
+
+	while (i >= 0)
+	{
+		_putchar(str[i]);
+		i--;
+	}
+	
+	_putchar('\n');
+}
