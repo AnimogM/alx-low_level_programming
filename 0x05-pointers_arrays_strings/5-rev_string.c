06443afcb2a96ff4896dd5bf29e61581d1f6d7a4
Gomina Maryam <80972735+AnimogM@users.noreply.github.com>
2022-03-21 15:06:09 +0100

    Update 5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,1 +1,23 @@
+#include "main.h"
 
+/**
+ *rev_string - prints reversed string;
+ *@s: string to be reversed;
+ */
+
+void rev_string(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	while (i > 0)
+	{
+		_putchar(s[i - 1]);
+		i--;
+	}
+	_putchar('\n');
+}
