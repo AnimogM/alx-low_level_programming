b27566d0beeee644f4e1b79aef7a80efec01e244
Gomina Maryam <gominamryam@gmail.com>
2022-03-21 04:14:57 -0700

    initial commit

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - reset to 98;
+ *
+ * Description - takes a pointer to an int as
+ * parameter and updates the value it points to
+ * 98;
+ *
+ * Return - 0;
+ */
+
+void reset_to_98(int *n)
+{
+	int *prt;
+
+	prt = *n;
+
+	prt = 98;
+}
