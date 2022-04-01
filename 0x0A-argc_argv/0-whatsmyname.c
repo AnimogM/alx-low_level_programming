2852d7eaecc12bf434ffa22908fa1389b8a5bd91
Gomina Maryam <gominamryam@gmail.com>
2022-04-01 04:40:21 -0700

    updated

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,10 +8,8 @@
  * Return: always 0;
  */
 
-int main(int argc__attribute__((unused)), char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	void argc;
-
 	printf("%s\n", argv[0]);
 	return (0);
 }
