0b8c9f0e0ae240c4d3d312c13a06514a28b04661
Gomina Maryam <80972735+AnimogM@users.noreply.github.com>
2022-04-01 13:45:14 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 {
 	int i, m = 1;
 
-	if (argc < 3)
+	if (argc != 3)
 	{
 		printf("Error\n");
 		return (1);
@@ -21,10 +21,9 @@ int main(int argc, char *argv[])
 	else
 	{
 
-		for (i = 1; i < argc; i++)
-		{
-			m *= atoi(argv[i]);
-		}
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		m = a * b;
 		printf("%d\n", m);
 		return (m);
 	}
