f2177d0205463854f5047920c2d4a5ffb956e55c
Gomina Maryam <80972735+AnimogM@users.noreply.github.com>
2022-04-01 13:50:39 +0100

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -18,12 +18,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		return (1);
 	}
-	else
-	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		m = a * b;
-		printf("%d\n", m);
-		return (m);
-	}
+	a = atoi(argv[1]);
+	b = atoi(argv[2]);
+	m = a * b;
+	printf("%d\n", m);
+	return (m);
 }
