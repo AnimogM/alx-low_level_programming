10b2d45c319fbfbbadecfc2b2aca30f9afbb0235
Gomina Maryam <gominamryam@gmail.com>
2022-04-04 21:28:02 -0700

    strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - copys a string
+ * @str: string to be copied
+ *
+ * Return: pointer to copied string
+ */
+
+char *_strdup(char *str)
+{
+	char *tmp;
+	int i, j;
+
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+	tmp = malloc(i * sizeof(char));
+
+	if (tmp == NULL)
+		return (NULL);
+
+	for (j = 0; j < i; j++)
+	{
+		tmp[j] = str[j];
+	}
+	return (tmp);
+}
