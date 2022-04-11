ac21e24450f7d03fc0d9b6fa58b83e22ae919746
Gomina Maryam <gominamryam@gmail.com>
2022-04-11 04:00:01 -0700

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - create a dog structure
+ * @name: dog name
+ * @owner: dog owner
+ * @age: dog age
+ */
+
+struct dog
+{
+	char *name;
+	char *owner;
+	double age;
+}
+
+#endif
