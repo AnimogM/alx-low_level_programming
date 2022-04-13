85145c20aac321acfd6e71ef993f88a0aeaddd7f
Gomina Maryam <gominamryam@gmail.com>
2022-04-13 08:11:58 -0700

    task0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include "function_pointers.h"
+
+/**
+ *print_name - print name
+ * @f: pointer to printer
+ * @name: name to be printer
+ *
+ *
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
